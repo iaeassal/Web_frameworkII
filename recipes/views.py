b299fd4dd927e046615339fdd84c0bb77ba6c1a1
ismail ali el assal <iaeassal@gmail.com>
2024-12-19 17:19:06 -0300

    Faker

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Ismail',
+        'recipes': [make_recipe() for _ in range(10)],
     })
+
+
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Ismail',
+        'recipe': make_recipe(),
     })
