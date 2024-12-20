5a8b39d089bcb56d0be85751e614dad1cb71eff0
ismail ali el assal <iaeassal@gmail.com>
2024-12-20 08:15:37 -0300

    Migrate

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
@@ -14,14 +18,9 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
-# EDITED
-# title description slug
-# preparation_time preparation_time_unit
-# servings servings_unit
-# preparation_step
-# preparation_step_is_html
-# created_at updated_at
-# is_published
-# cover
-# category (Relação)
-# Author (Relação)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
