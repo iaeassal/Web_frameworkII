ebfb9d1e50c6d934f1f920ece4eb405ac6f60fc1
ismail ali el assal <iaeassal@gmail.com>
2024-12-24 10:32:21 -0300

    IsValidParaSalvarDados

--- authors/forms.py
+++ authors/forms.py
@@ -1,3 +1,4 @@
+import re
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
@@ -12,6 +13,16 @@ def add_placeholder(field, placeholder_val):
     add_attr(field, 'placeholder', placeholder_val)
 
 
+def strong_password(password):
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+    if not regex.match(password):
+        raise ValidationError((
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be '
+            'at least 8 characters.'
+        ),
+            code='invalid'
+        )
 class RegisterForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -33,7 +44,8 @@ class RegisterForm(forms.ModelForm):
             'Password must have at least one uppercase letter, '
             'one lowercase letter and one number. The length should be '
             'at least 8 characters.'
-        )
+        ),
+        validators=[strong_password]
     )
     password2 = forms.CharField(
         required=True,
@@ -100,10 +112,13 @@ class RegisterForm(forms.ModelForm):
             )
 
         return data
+
     def clean(self):
         cleaned_data = super().clean()
+
         password = cleaned_data.get('password')
         password2 = cleaned_data.get('password2')
+
         if password != password2:
             password_confirmation_error = ValidationError(
                 'Password and password2 must be equal',
